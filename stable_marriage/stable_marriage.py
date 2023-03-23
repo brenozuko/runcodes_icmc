bae718a18104eba3c8eaf7b64ca3ec1a1bfe7ffe
Breno Zukowski <brenozukotech@gmail.com>
2023-03-23 10:32:26 -0300

    First problem stable_marriage

---
+++ stable_marriage/stable_marriage.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+
+def stableMarriage(prefer):
+    pass
+
+
+for j in range(t):
+    n = int(input())
+    prefer = []
+
+    for i in range(2 * n):
+        prefer.append(input().split())
+
+    print(prefer)
